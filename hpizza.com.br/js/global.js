5356c20da824b96d64dc063f468bdcbdc611792b
orlandolilo <83991283+orlandoficr@users.noreply.github.com>
2021-05-12 19:28:06 -0300

    pag-inicial-patch2.0

--- hpizza.com.br/js/global.js
+++ hpizza.com.br/js/global.js
@@ -1,1 +1,17 @@
 M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
+
+  //Constrói a URL depois que o DOM estiver pronto
+document.addEventListener("DOMContentLoaded", function() {
+    //conteúdo que será compartilhado: Título da página + URL
+    var conteudo = encodeURIComponent(document.title + " " + window.location.href);
+    //altera a URL do botão
+    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+}, false);

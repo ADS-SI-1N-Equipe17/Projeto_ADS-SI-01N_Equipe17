004b2554fcf159a1bf600b177fbf1d958886e6ee
orlandolilo <83991283+orlandoficr@users.noreply.github.com>
2021-05-12 23:24:10 -0300

    js-mudanças

--- hpizza.com.br/js/global.js
+++ hpizza.com.br/js/global.js
@@ -1,17 +1,8 @@
+//Js Materialize
 M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
-    });
-  });
-
-  //Constrói a URL depois que o DOM estiver pronto
-document.addEventListener("DOMContentLoaded", function() {
-    //conteúdo que será compartilhado: Título da página + URL
-    var conteudo = encodeURIComponent(document.title + " " + window.location.href);
-    //altera a URL do botão
-    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+//Botão de compartilhar pagina no WhatsApp
+document.addEventListener("DOMContentLoaded", function () {
+  var conteudo = encodeURIComponent(document.title + " " + window.location.href);
+  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
 }, false);

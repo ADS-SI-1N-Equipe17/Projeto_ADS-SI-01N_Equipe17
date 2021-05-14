1b0ac02b373df4a6603598487a6e4e1ceec82738
Orlando Ferreria <83991283+orlandoficr@users.noreply.github.com>
2021-05-14 18:46:31 -0300

    atualizando

--- darkkitchen.com.br/js/global.js
+++
@@ -1,8 +0,0 @@
-//Js Materialize
-M.AutoInit();
-
-//Botão de compartilhar pagina no WhatsApp
-document.addEventListener("DOMContentLoaded", function () {
-  var conteudo = encodeURIComponent(document.title + " " + window.location.href);
-  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
-}, false);

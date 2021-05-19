3deb4be5ea1a1687f12cf74b4dad0538b92ea21e
Orlando Ferreria <83991283+orlandoficr@users.noreply.github.com>
2021-05-19 15:45:15 -0300

    modal-login

--- hpizza.com.br/js/global.js
+++ hpizza.com.br/js/global.js
@@ -1,7 +1,11 @@
 //Js Materialize
 M.AutoInit();
 
-//Botão de compartilhar pagina no WhatsApp
+/*$(document).ready(function(){
+  $('.modal').modal();
+});*/
+
+//Compartilhar pagina no WhatsApp
 document.addEventListener("DOMContentLoaded", function () {
   var conteudo = encodeURIComponent(document.title + " " + window.location.href);
   document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;

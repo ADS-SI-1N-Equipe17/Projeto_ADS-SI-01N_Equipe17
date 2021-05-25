4d4000e880af89112c6db4f381a599e31e5ecdac
Orlando Ferreria <83991283+orlandoficr@users.noreply.github.com>
2021-05-24 21:32:51 -0300

    .

--- hpizza.com.br/js/global.js
+++ hpizza.com.br/js/global.js
@@ -6,3 +6,4 @@ document.addEventListener("DOMContentLoaded", function () {
   var conteudo = encodeURIComponent(document.title + " " + window.location.href);
   document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
 }, false);
+

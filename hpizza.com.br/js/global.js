0f0221deaa678c94149da29051af4f47038410bf
Orlando Ferreria <83991283+orlandoficr@users.noreply.github.com>
2021-06-08 15:12:54 -0300

    .

--- hpizza.com.br/js/global.js
+++ hpizza.com.br/js/global.js
@@ -288,13 +288,6 @@ function addcarrinho(x, y) {
 
   console.log("Valor total: " + total.toFixed(2))
   sessionStorage.setItem("produtosCarrinho", JSON.stringify(carrinho));
-  
-  var ing = total;
-  var lista = document.getElementById("lista").innerHTML;
-  lista = lista + "<li>" + ing + "</li>";
-  
-  document.getElementById("lista").innerHTML = lista;
 
 }
 
-//incrementar pagina de carrinho
